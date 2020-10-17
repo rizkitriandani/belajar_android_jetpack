cd5b4c7d7127d6b6158ca08e50c1b165b973332d
rizkitriandani <rizkitriandani@yahoo.co.id>
2020-10-17 22:09:37 +0700

    Change layout a little bit.

--- app/src/main/java/com/belajar/androidjetpack/MainActivity.kt
+++ app/src/main/java/com/belajar/androidjetpack/MainActivity.kt
@@ -2,10 +2,35 @@ package com.belajar.androidjetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textAngka: TextView
+    private lateinit var btnTambah: Button
+    private lateinit var btnKurang: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textAngka = findViewById(R.id.textAngka)
+        btnTambah = findViewById(R.id.btnTambah)
+        btnKurang = findViewById(R.id.btnKurang)
+
+        var angka:Int = 0
+        textAngka.text = angka.toString()
+
+        btnTambah.setOnClickListener{
+            angka++
+            textAngka.text = angka.toString()
+        }
+
+        btnKurang.setOnClickListener{
+            angka--
+            textAngka.text = angka.toString()
+        }
+
     }
 }
